1ebf33835f495a9433fc86e81011982d67b55983
ljholiday <lonn@ljholiday.com>
2026-02-07 13:22:42 -0800

    Prefer PHP_SELF for base path detection

--- src/MarkdownEditor/Http/Url.php
+++ src/MarkdownEditor/Http/Url.php
@@ -2,10 +2,26 @@
 
 namespace MarkdownEditor\Http;
 
+use MarkdownEditor\Config\Config;
+
 class Url
 {
     public static function basePath(): string
     {
+        $override = Config::get('BASE_PATH');
+        if (!empty($override)) {
+            $override = '/' . trim($override, '/');
+            return $override === '/' ? '' : $override;
+        }
+
+        $phpSelf = $_SERVER['PHP_SELF'] ?? '';
+        if (!empty($phpSelf)) {
+            $dir = rtrim(str_replace('\\', '/', dirname($phpSelf)), '/');
+            if ($dir !== '/' && $dir !== '.') {
+                return $dir;
+            }
+        }
+
         $scriptFilename = $_SERVER['SCRIPT_FILENAME'] ?? '';
         $docRoot = $_SERVER['DOCUMENT_ROOT'] ?? '';
 
