4f85f9bb964c4a1804ad9e37a13f21742ed636e2
ljholiday <lonn@ljholiday.com>
2026-02-07 16:01:22 -0800

    Extend session lifetime to reduce logouts

--- src/MarkdownEditor/Auth/SessionAuth.php
+++ src/MarkdownEditor/Auth/SessionAuth.php
@@ -9,6 +9,15 @@ class SessionAuth
     public function __construct()
     {
         if (session_status() === PHP_SESSION_NONE) {
+            $lifetime = 60 * 60 * 24 * 30;
+            ini_set('session.gc_maxlifetime', (string)$lifetime);
+            session_set_cookie_params([
+                'lifetime' => $lifetime,
+                'path' => '/',
+                'secure' => (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'),
+                'httponly' => true,
+                'samesite' => 'Lax'
+            ]);
             session_start();
         }
     }
